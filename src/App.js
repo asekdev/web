73be4f282e1bbb7d37132a6d0f2607e91331f6d1
asekdev <andy.sek94@gmail.com>
2019-06-04 21:47:28 +1000

    revamp

--- src/App.js
+++ src/App.js
@@ -1,62 +1,104 @@
-import React, { Component } from 'react';
-import './App.css';
-import './assets/css/bootstrap.css';
-import Home from './_components/Home';
-import Particles from 'react-particles-js';
+import React, { Component } from "react";
+import "./App.css";
+import "./assets/css/bootstrap.css";
+import Home from "./_components/Home";
+import Particles from "react-particles-js";
 
 class App extends Component {
-
 	constructor(props) {
 		super(props);
 	}
 
-  render() {
-    return (
-      <div className="App">
-		
-        <div id="particles">
-			<Particles
-			params={{
-			"particles": {
-				"number": {
-					"value": 100,
-				},
-				"interactivity": {
-					"events": {
-						"onhover": {
-							"enable": false,
-							"mode": "repulse"
-						},
-						"resize": true
-					},
-				},
-				"line_linked": {
-					"enable": true,
-					"opacity": 0.04
-				},
-				"move": {
-					"direction": "none",
-					"random": true,
-					"speed": 2
-				},
-				"size": {
-					"value": 2
-				},
-				"opacity": {
-					"anim": {
-						"enable": true,
-						"speed": 2,
-						"opacity_min": 0.05
-					}
-				}
-			},
-			"retina_detect": true
-		}} />
-		<Home />		
-		</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div id="particles">
+					<Particles
+						params={{
+							particles: {
+								number: {
+									value: 100
+								},
+								interactivity: {
+									events: {
+										onhover: {
+											enable: false,
+											mode: "repulse"
+										},
+										resize: true
+									}
+								},
+								line_linked: {
+									enable: true,
+									opacity: 0.04
+								},
+								move: {
+									direction: "none",
+									random: true,
+									speed: 2
+								},
+								size: {
+									value: 2
+								},
+								opacity: {
+									anim: {
+										enable: true,
+										speed: 2,
+										opacity_min: 0.05
+									}
+								}
+							},
+							retina_detect: true
+						}}
+					/>
+				</div>
+				<div>
+					<Home />
+				</div>
+
+				<div id="particlesTwo">
+					<Particles
+						params={{
+							particles: {
+								number: {
+									value: 250
+								},
+								interactivity: {
+									events: {
+										onhover: {
+											enable: false,
+											mode: "repulse"
+										},
+										resize: true
+									}
+								},
+								line_linked: {
+									enable: false,
+									opacity: 0.04
+								},
+								move: {
+									direction: "none",
+									random: true,
+									speed: 1
+								},
+								size: {
+									value: 1
+								},
+								opacity: {
+									anim: {
+										enable: true,
+										speed: 1,
+										opacity_min: 0.5
+									}
+								}
+							},
+							retina_detect: true
+						}}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/_components/Experience.js
+++ src/_components/Experience.js
@@ -1,27 +1,62 @@
-import React, {Component} from 'react';
-import '../assets/css/custom.css';
-import ReactTyped from 'react-typed';
+import React, { Component } from "react";
+import "../assets/css/custom.css";
+import ReactTyped from "react-typed";
 
 class Experience extends Component {
-      
-    render() {
-        return(
-            <div className="experience-wrapper">
-            <h4 className="experience-text">
-                <ReactTyped strings={["<span style='color:#eb3b5a'>UI/UX</span>",
-                                      "<span style='color:#ffdd59'>Frontend</span>",
-                                      "<span style='color:#22a7f0'>Backend</span>",
-                                      "<span style='color:#55E6C1'>Full Stack Developer</span>",     
-                                    ]}
-                typeSpeed={50}
-                backSpeed={70}
-                backDelay={2}
-                showCursor={false}
-                startDelay={1800}
-                smartBackspace/></h4>
-            </div>
-        );
-    }
+	constructor(props) {
+		super(props);
+
+		this.delayedStartUp = this.delayedStartUp.bind(this);
+		this.hideCursorIcon = this.hideCursorIcon.bind(this);
+		this.state = {
+			show: false,
+			hideCursor: true
+		};
+	}
+
+	componentDidMount() {
+		this.delayedStartUp();
+		this.hideCursorIcon();
+	}
+
+	delayedStartUp() {
+		setTimeout(() => {
+			this.setState({ show: true });
+		}, 1500);
+	}
+
+	hideCursorIcon() {
+		setTimeout(() => {
+			this.setState({ hideCursor: false });
+		}, 4000);
+	}
+
+	render() {
+		return (
+			<div className="experience-wrapper">
+				{this.state.show && (
+					<h4 className="experience-text">
+						<ReactTyped
+							strings={[
+								"<span style='color:#ffdd59'>Frontend</span>",
+								"<span style='color:#22a7f0'>Backend</span>",
+								"<span style='color:#55E6C1'>Full Stack Developer</span>"
+							]}
+							typeSpeed={70}
+							backSpeed={70}
+							backDelay={2}
+							showCursor={this.state.hideCursor}
+							onComplete={() => {
+								this.setState({ hideCursor: false });
+							}}
+							startDelay={1000}
+							smartBackspace
+						/>
+					</h4>
+				)}
+			</div>
+		);
+	}
 }
 
 export default Experience;
